ca74449245111058ffb7a6baad27e92ab05a317b
johnny_en <johnny_2008@aliyun.com>
2022-10-20 19:55:04 +0800

    实现 isProxy 功能

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { isReadonly, readonly } from "../reactive";
+import { isProxy, isReadonly, readonly } from "../reactive";
 
 describe("readonly", () => {
   it("happy path", () => {
@@ -10,6 +10,7 @@ describe("readonly", () => {
     expect(isReadonly(original)).toBe(false);
     expect(isReadonly(observed.bar)).toBe(true);
     expect(isReadonly(original.bar)).toBe(false);
+    expect(isProxy(observed)).toBe(true);
   });
 
   it("warn then call set", () => {
