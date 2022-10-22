0af0ce8cab85492fb10f9186fff7b36419509c35
johnny_en <johnny_2008@aliyun.com>
2022-10-22 22:22:17 +0800

    添加公共函数

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,7 @@
 export const extend = Object.assign;
 
 export const isObject = (value) => value !== null && typeof value === "object";
+
+export const hasChange = (value, newValue) => {
+  return !Object.is(value, newValue);
+};
