e530384769cfac23ad4adb7468540705f7f58ee6
johnny_en <johnny_2008@aliyun.com>
2022-10-22 22:23:58 +0800

    实现 ref

---
+++ src/reactivity/ref.ts
@@ -0,0 +1,39 @@
+import { hasChange, isObject } from "../shared";
+import { isTracking, trackEffects, triggerEffect } from "./effect";
+import { reactive } from "./reactive";
+
+class RefImpl {
+  private _value: any;
+  private _rawValue: any;
+  public dep;
+  constructor(value) {
+    this._rawValue = value;
+    this._value = convert(value);
+    this.dep = new Set();
+  }
+  get value() {
+    trackRefValue(this);
+    return this._value;
+  }
+  set value(newValue) {
+    if (hasChange(this._rawValue, newValue)) {
+      this._rawValue = newValue;
+      this._value = convert(newValue);
+      triggerEffect(this.dep);
+    }
+  }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
+
+function trackRefValue(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep);
+  }
+}
+
+export function ref(value) {
+  return new RefImpl(value);
+}
