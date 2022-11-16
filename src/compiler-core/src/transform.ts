7c3ece2affbab218acbf1bf8fbf5d0019c3bbc6a
johnny_en <johnny_2008@aliyun.com>
2022-11-17 00:18:16 +0800

    实现代码生成 string 类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,7 +1,13 @@
-export function transform(root, options) {
+export function transform(root, options = {}) {
   const context = createTransformContext(root, options);
   // 遍历 - 深度优先搜索
   traverseNode(root, context);
+
+  createRootCodegen(root);
+}
+
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0];
 }
 
 function traverseNode(node: any, context: any) {
